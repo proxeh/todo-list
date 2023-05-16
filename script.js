c8be415865f3616fcbff4770b4cade90ebaf5577
Jamie Shaw <proxeh@gmail.com>
2023-05-16 23:34:43 +0100

    Basic functionality
    Basic functions in place

--- script.js
+++ script.js
@@ -4,6 +4,8 @@
 const itemForm = document.getElementById("item-form");
 const itemInput = document.getElementById("item-input");
 const itemList = document.getElementById("item-list");
+const clearButton = document.getElementById("clear");
+const itemFilter = document.getElementById("filter");
 
 function addItem(e) {
 	e.preventDefault();
@@ -22,7 +24,10 @@ function addItem(e) {
 	const button = createButton("remove-item btn-link text-red");
 	li.appendChild(button);
 
+	// Add li to DOM
 	itemList.appendChild(li);
+
+	resetUI();
 
 	itemInput.value = "";
 }
@@ -44,5 +49,53 @@ function createIcon(classes) {
 	return icon;
 }
 
+function filterItems(e) {
+	const items = itemList.querySelectorAll("li");
+	const text = e.target.value.toLowerCase();
+
+	items.forEach((item) => {
+		const itemName = item.firstChild.textContent.toLowerCase();
+
+		if (itemName.indexOf(text) != -1) {
+			item.style.display = "flex";
+		} else {
+			item.style.display = "none";
+		}
+	});
+}
+
+function removeItem(e) {
+	if (e.target.parentElement.classList.contains("remove-item")) {
+		if (confirm("Are you sure?")) {
+			e.target.parentElement.parentElement.remove();
+			resetUI();
+		}
+	}
+}
+
+function clearItems(e) {
+	while (itemList.firstChild) {
+		itemList.removeChild(itemList.firstChild);
+	}
+	resetUI();
+}
+
+function resetUI() {
+	const items = itemList.querySelectorAll("li");
+
+	if (items.length === 0) {
+		itemFilter.style.display = "none";
+		clearButton.style.display = "none";
+	} else {
+		itemFilter.style.display = "block";
+		clearButton.style.display = "block";
+	}
+}
+
 // Listeners
 itemForm.addEventListener("submit", addItem);
+itemFilter.addEventListener("input", filterItems);
+itemList.addEventListener("click", removeItem);
+clearButton.addEventListener("click", clearItems);
+
+resetUI();
